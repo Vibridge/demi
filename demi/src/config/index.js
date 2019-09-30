5ea2a40b6b86536ac093a5efc3e005bfaca0c113
reader <1318984137@qq.com>
2019-09-30 18:35:10 +0800

    a

---
+++ demi/src/config/index.js
@@ -0,0 +1,19 @@
+export default {
+
+    /**
+     * @description 配置显示在浏览器标签的系统名称
+     */
+    title: '后台管理系统',
+
+    /**
+     * @description 接口请求基础路径
+     */
+    baseUrl: 'http://produce.jmzhipin.com',
+    // baseUrl: 'http://app.jmzhipin.com',
+    suffixUrl: '/dominator',
+
+    /**
+     * @description token在Cookie中存储的天数，默认90天
+     */
+    cookieExpires: 90
+}
